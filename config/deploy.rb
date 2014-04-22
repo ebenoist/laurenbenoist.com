531f28ab2965ca07d8925972e61d2fa59907762a
Erik Benoist <ebenoist@gmail.com>
2014-04-21 22:28:38 -0500

    new favicon

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,51 +6,26 @@ require "bundler/capistrano"
 set :application, Application.config["application_name"]
 set :domain,      Application.config["domain"]
 set :repository,  Application.config["repository"]
-set :deploy_to,   "/home/ubuntu/#{application}"
+set :deploy_to,   "/home/deploy/#{application}"
 set :use_sudo,    false
 set :scm,         "git"
 set :user,        Application.config["deploy_user"]
 set :deploy_via, :remote_cache
 set :normalize_asset_timestamps, false
 
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
+
 role :app, domain
 
-#SET UP RVM
-set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
-set :rvm_install_ruby_params, '--1.9'      # for jruby/rbx default to 1.9 mode
-set :rvm_install_pkgs, %w[libyaml openssl] # package list from https://rvm.io/packages
-set :rvm_install_ruby_params, '--with-opt-dir=/usr/local/rvm/usr' # package support
-
-after 'deploy:setup', 'deploy:upload_config' # upload config to shared
-after 'deploy:create_symlink', 'deploy:link_config' # link the application.yml in shared
-
 namespace :deploy do
-  task :upload_config do
-    application_config = File.read(Application.root + "/config/application.yml")
-    run("mkdir -p #{shared_path}/config")
-    put(application_config, "#{shared_path}/config/application.yml")
-  end
-
-  task :link_config do
-    run("ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml")
-  end
-
-  task :force_restart do
-    run("cd #{current_path} && bundle exec rake force_restart")
-  end
-
   task :start do
-    run("cd #{current_path} && bundle exec rake start")
   end
 
   task :stop do
-    run("cd #{current_path} && bundle exec rake stop")
   end
 
   task :restart do
-    run("cd #{current_path} && bundle exec rake restart")
   end
 end
-
-
-
